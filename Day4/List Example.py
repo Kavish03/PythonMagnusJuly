d44ba31d41496dddfb44bc5550c75dc1311897bf
mkarth3 <mkarth3@hotmail.com>
2023-07-28 19:23:56 -0400

    Day4_July_2023_new

--- Day4/List Example.py
+++ Day4/List Example.py
@@ -5,9 +5,10 @@ print(a1[0:3])
 print(a1[3:6])
 print(a1[2:])
 print(a1[:5])
-a1.append(120)
 print(a1)
-a1.insert(3,'hi')
+a1.append(120) # Appending the data to list
+print(a1)
+a1.insert(3,'hi') # inserting the value at the specific position
 print(a1)
 a1[6]="welcome" # replacing the value at specific position
 print(a1)
@@ -16,4 +17,5 @@ print(a1)
 a1.pop()
 print(a1)
 a1.pop(4)
+print(a1)
 print(a1)
