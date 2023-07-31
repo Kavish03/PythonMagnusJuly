692aae6d0b92bb729215b32313569070012955ab
mkarth3 <mkarth3@hotmail.com>
2023-07-31 19:10:36 -0400

    Day5_July27_2023

---
+++ Day5/RangeExample.py
@@ -0,0 +1,2 @@
+a1= range(1,11,1) #range(start,stop,step)
+print(a1[5])
---
+++ Notes/Day5txt.py
@@ -0,0 +1,38 @@
+collections
+       List
+            ordered Data
+            Mutable
+            Index
+            []
+          Tuple
+             ordered collections
+             Immutable
+             ()
+          set
+             unordered collection
+             Mutable with restriction
+             {}
+          Dictionary
+                stored the pairs of value(key,value)
+                Mutable
+                ordered collection
+                {}
+
+control statements
+
+conditional statements
+        simple if
+        if else
+        else if
+        nested if
+
+Looping statement
+         For loop
+         while loop
+
+
+
+
+
+
+
