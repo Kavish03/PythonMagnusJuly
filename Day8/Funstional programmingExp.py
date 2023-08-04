998764c7e891382e105a5782c782135c67e21d0d
mkarth3 <mkarth3@hotmail.com>
2023-08-04 18:54:00 -0400

    Day7_Aug1_2023

---
+++ Day8/Funstional programmingExp.py
@@ -0,0 +1,14 @@
+Functional programming or standalone programming
+
+object oriented programming
+Every program will be treated as a real world entity(example)
+1) class
+2) object
+3) inheritance
+4) polymorphism
+5) Abstraction
+
+
+Class and object
+Blueprint: Model or design of an application
+Variables and Functions
